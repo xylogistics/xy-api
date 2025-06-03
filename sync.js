0d6e0b0934d51e0b88147ddfc3d759b9b51de48d
Thomas Coats <thomas.coats@gmail.com>
2025-06-03 16:16:49 +1200

    entity_id

--- sync.js
+++ sync.js
@@ -610,6 +610,7 @@ export default (ws, fn) => {
     order_sanitise: o => {
       if (o == null) return null
       return {
+        entity_id: o.entity_id,
         order_id: o.order_id,
         order_external_id: o.order_external_id,
         order_group_id: o.order_group_id,
