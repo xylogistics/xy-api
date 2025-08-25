1ad08a458894b11350f349b423dc2ab71a60357a
Bryce Nemhauser <bryce@y5.nz>
2025-08-26 10:53:41 +1200

    parametise library call

--- user-cookie-upgrade.js
+++ user-cookie-upgrade.js
@@ -1,10 +1,10 @@
 import cookies from 'cookies'
 
-export default () =>
+export default cookieSecret =>
   async ({ httpServer, jwt }) => {
     httpServer.on('upgrade', req => {
       try {
-        const cookieJar = cookies(req, {}, { keys: [process.env.XY_COOKIE_SECRET] })
+        const cookieJar = cookies(req, {}, { keys: [cookieSecret] })
         const token = cookieJar.get('xy_auth_token', { signed: true })
         if (!token) return
         req.authorization = jwt.verify(token)
